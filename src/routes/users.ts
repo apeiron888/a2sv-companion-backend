b1398046014d9f4c38568e5047de1793d3834d09
apeiron888 <aelafgetaneh@gmail.com>
2026-02-04 01:50:55 +0300

    feat: add requireManager middleware to user update route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { UserModel } from "../models/user.js";
+import { requireManager } from "../middleware/auth.js";
 
 const router = Router();
 
@@ -20,6 +21,16 @@ router.patch("/users/me", (_req, res) => {
   const update = _req.body || {};
   UserModel.findByIdAndUpdate(userId, update, { new: true, upsert: true })
     .then((user) => res.json(user))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.patch("/users/:id", requireManager, (_req, res) => {
+  const update = _req.body || {};
+  UserModel.findByIdAndUpdate(_req.params.id, update, { new: true })
+    .then((user) => {
+      if (!user) return res.status(404).json({ error: "User not found" });
+      return res.json(user);
+    })
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
